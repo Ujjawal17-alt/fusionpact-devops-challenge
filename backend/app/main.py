41b87533204d2f010343bb6fa7dc3143d90b219f
root <root@ip-172-31-32-185.ap-south-1.compute.internal>
2025-10-15 09:06:37 +0000

    Add Dockerfiles, update backend main.py, add requirements, and monitoring

--- backend/app/main.py
+++ backend/app/main.py
@@ -36,3 +36,14 @@ async def get_users():
     Read user data from json file
     """
     return services.read_usersdata()
+from fastapi import FastAPI
+from prometheus_fastapi_instrumentator import Instrumentator
+
+app = FastAPI()
+
+Instrumentator().instrument(app).expose(app)
+
+@app.get("/")
+def read_root():
+    return {"message": "Fusionpact DevOps Challenge Backend Running"}
+
